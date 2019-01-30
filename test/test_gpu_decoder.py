79f8989111600265a60bb9c82870fc0b9c9df91f
WENBO JIANG <jian0085@e.ntu.edu.sg>
2019-01-30 17:07:24 +0800

    change test_gpu_decoder.py

--- test/test_gpu_decoder.py
+++ test/test_gpu_decoder.py
@@ -1,10 +1,24 @@
-import PyHysiaDec
+import sys
+sys.path.insert(0, "../build")
+import PyDecoder
 import cv2
-import time                                                                                                                                                                   
- 
- 
+import time
+
 if __name__ == "__main__":
     video_path = "bigbang.mp4"
+
+    # Test GPU decoder
+    dec = PyDecoder.Decoder("GPU:0")
+    dec.ingestVideo(video_path)
+    dec.decode()
+    tick = time.time()
+    while True:
+        frame = dec.fetchFrame()
+        if frame.size == 0:
+            break
+    tock = time.time()
+    print("time taken for gpu video reader is %.3f" % (tock - tick))
+
     # Test cv2 video capture
     cap = cv2.VideoCapture(video_path)
     tick = time.time()
@@ -14,15 +28,4 @@ if __name__ == "__main__":
             break
     tock = time.time()
     print("time taken for cv2 video reader is %.3f" % (tock - tick))
-
-    # Test GPU decoder
-    dec = PyHysiaDec.GPUDecoder(1)
-    dec.IngestVideo(video_path)
-    tick = time.time()
-    while True:
-        frame = dec.DecodeFrames()
-        if frame.size == 0:
-            break
-    tock = time.time()
-    print("time taken for gpu video reader is %.3f" % (tock - tick))
 
